33e33b80264325ccf2ae93be1d3634d180bb1208
NadyaGus <ftotkexit@gmail.com>
2024-09-04 18:26:34 +0300

    feat: replace button to link in header

--- homework_1/src/shared/header/Header.tsx
+++ homework_1/src/shared/header/Header.tsx
@@ -3,17 +3,14 @@ import type { ReactNode } from 'react';
 import classes from './Header.module.css';
 
 export function Header(): ReactNode {
-  const handleClick = (): void => {
-    // TODO: add modal here
-    alert('О задании');
-  };
-
   return (
     <header className={classes.header}>
-      <h1 className={classes.title}>Y_LAB Homework</h1>
-      <button className={classes.button} onClick={handleClick}>
-        О задании
-      </button>
+      <div className={classes.wrapper}>
+        <h1 className={classes.title}>Y_LAB Homework</h1>
+        <a className={classes.link} href="https://github.com/NadyaGus" rel="noreferrer" target="_blank">
+          By NadyaGus
+        </a>
+      </div>
     </header>
   );
 }
