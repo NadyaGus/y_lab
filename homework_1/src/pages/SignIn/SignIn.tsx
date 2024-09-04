2fe3076e25c7b9ae9437d11f02516affc1f8d0b8
NadyaGus <ftotkexit@gmail.com>
2024-09-04 18:08:39 +0300

    feat: add validation for inputs

--- homework_1/src/pages/SignIn/SignIn.tsx
+++ homework_1/src/pages/SignIn/SignIn.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 
+import { clsx } from 'clsx';
+
 import { signIn } from './api/Api';
 
 import classes from './SignIn.module.css';
@@ -12,7 +14,11 @@ interface Inputs {
 }
 
 export function SignIn(): ReactNode {
-  const { handleSubmit, register } = useForm<Inputs>();
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     signIn({ email: data.email, password: data.password });
   };
@@ -21,14 +27,34 @@ export function SignIn(): ReactNode {
     <div className={classes.container}>
       <form className={classes.form} onSubmit={(event) => void handleSubmit(onSubmit)(event)}>
         <h1 className={classes.title}>Войти</h1>
+
         <label className={classes.label}>
           Email
-          <input className={classes.input} placeholder="Введите email" type="text" {...register('email')} />
+          <input
+            className={clsx(classes.input, { [classes.invalid]: errors.email })}
+            placeholder="Введите email"
+            type="text"
+            {...register('email', {
+              pattern: { message: 'Некорректный email', value: /^\S+@\S+$/i },
+              required: 'Обязательное поле',
+            })}
+            aria-invalid={errors.email ? 'true' : 'false'}
+          />
+          <p className={clsx(classes.error, { [classes.hidden]: !errors.email })}>{errors.email?.message}</p>
         </label>
+
         <label className={classes.label}>
           Пароль
-          <input className={classes.input} placeholder="Введите пароль" type="password" {...register('password')} />
+          <input
+            aria-invalid={errors.email ? 'true' : 'false'}
+            className={clsx(classes.input, { [classes.invalid]: errors.password })}
+            placeholder="Введите пароль"
+            type="password"
+            {...register('password', { required: 'Обязательное поле' })}
+          />
+          <p className={clsx(classes.error, { [classes.hidden]: !errors.password })}>{errors.password?.message}</p>
         </label>
+
         <button className={classes.button} type="submit">
           Войти
         </button>
